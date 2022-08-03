c9bc9692c1ce81a634717089ef6003189827dc2b
CoffeeLatteShop <miraicchi.exe@gmail.com>
2022-08-03 21:09:59 +0700

    Test Deploy

---
+++ src/pages/homepage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+// import { Nav, Button } from 'react-bootstrap'
+// import { Link } from 'react-router-dom'
+import cssModules from './homepage.module.css'
+
+export default function Homepage() {
+    <>
+        <div className={cssModules}>
+            <h1>Hi</h1>
+            <h1>Hi</h1>
+            <h1>Hi</h1>
+            <h1>Hi</h1>
+            <h1>Hi</h1>
+            <h1>Hi</h1>
+            <h1>Hi</h1>
+        </div>
+    </>
+}
